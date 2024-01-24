379c2158ab9150ec6e7e13d4518d93118572be32
João Mário <joaomariowd@gmail.com>
2024-01-23 21:18:42 -0300

    styling logo

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -6,7 +6,7 @@ const Header = () => {
     <header className="p-2 bg-primary text-white shadow-md shadow-neutral-300 border-b border-0 border-primary-dark">
       <div className='w-20'>
         <Link to='/'>
-          <img className='rounded-md p-2 border border-primary-dark' src={appLogo} alt="App Logo" />
+          <img className='rounded-md p-2 border bg-primary-light border-gold' src={appLogo} alt="App Logo" />
         </Link>
       </div>
     </header>
